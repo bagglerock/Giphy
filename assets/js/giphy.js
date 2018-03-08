ead84be6ad20ee61a2a5e8c1edfc2115d7f5f03c
Oscar Villalta <bagglerock@Oscars-MacBook-Pro.local>
2018-03-08 02:50:06 -0500

    initial files

---
+++ assets/js/giphy.js
@@ -0,0 +1,21 @@
+//**   Array of search terms   **//
+
+
+
+
+
+//**   Variables   **//
+
+var apiKey = "brX9hakQgeVuceuukT4rEEa4B1xHod1Y";
+
+
+
+
+
+//**   Functions   **//
+
+
+
+
+
+//**   Event Listeners   **//

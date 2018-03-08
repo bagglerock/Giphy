bcb0bdaf0abd0da3477f8e9db62e917ab52e3f17
Oscar Villalta <bagglerock@Oscars-MacBook-Pro.local>
2018-03-08 03:18:48 -0500

    have the basic function working

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,7 +1,6 @@
 //**   Array of search terms   **//
 
-
-
+var searchTerms = ["cat", "dog", "Garfield", "Odie"];
 
 
 //**   Variables   **//
@@ -11,11 +10,52 @@ var apiKey = "brX9hakQgeVuceuukT4rEEa4B1xHod1Y";
 
 
 
-
 //**   Functions   **//
 
+function doSearch(search) {
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + apiKey + "&tag=" + search;
+    //  Ajax Call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+            console.log(response);
+
+
+        });
+}
+
+function renderButtons () {
+    //  Get the stuff from the array and make the buttons
+    var button = $("<button>");
+
+}
+
+function showResults () {
+    // Show the search results
+}
 
 
 
 
 //**   Event Listeners   **//
+
+$(document).ready(function() {
+
+    $("#add-search").on("click", function() {
+        event.preventDefault();
+        var input = $("#search-input").val().trim();
+        $("#search-input").val("");
+        doSearch(input);
+    });
+
+
+
+
+
+
+
+});
+
+console.log("test");
